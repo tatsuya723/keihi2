f621c6b084c11a47d64be51d497c137c423214d1
T.Okabayashi <e5408@gm.kochi-ct.jp>
2020-04-25 03:26:48 +0900

    no message

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
     <h1 class="text-light">Pure CSS Animated Gradient Background</h1>
     <h1 class="text-light">出来事を共有しましょう</h1>
 
-    <form name="form1" method="post" action="index.php" enctype="multipart/form-data" class="box17">
+    <form name="form1" method="post" action="index.php" enctype="multipart/form-data" class="box11">
     <input type="hidden" name="MAX_FILE_SIZE" value="10000000">
-    画像を選択：<input type="file" name="upfile"><br>
-    コメント<br>：<textarea name="comment" rows="5" cols="50"></textarea><br>
-    パスワード(4ケタ)：<input type="text" name="pass" maxlength="4"><br>
+    画像を選択<br><input type="file" name="upfile"><br>
+    <br>コメント<br>：<textarea name="comment" rows="5" cols="50"></textarea><br>
+    <br>パスワード(4ケタ)<br><input type="text" name="pass" maxlength="4"><br>
     <input type="submit" value="投稿する"><br>
 
 
