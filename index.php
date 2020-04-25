80eebe1ee70fa04c4a34148b07c782838aa6eea8
T.Okabayashi <e5408@gm.kochi-ct.jp>
2020-04-25 17:50:05 +0900

    no message

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ foreach($rs as $row){
 ?>
         <section class="box.17">
             <IMG class="card-img" src="<?=$row["link"]?>"><br>
-            <p ><font class="card-text0">name:<?=$row["nam"]?></font>　　　　　<font class="card-text1">data:<?=$row["ymd"]?></font></p>
+            <p><font class="card-text0">name:<?=$row["nam"]?></font>　　　　　<font class="card-text1">data:<?=$row["ymd"]?></font></p>
             <p class="card-text2"><?=$row["com"]?></p><br>
 
         </section>
